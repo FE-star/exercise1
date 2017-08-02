6c615480612462a04eeb6f1b5f653797cc3ae0dc
式燕 <wenjian.ywj@alibaba-inc.com>
2017-08-02 22:31:58 +0800

    update

--- test/test.js
+++ test/test.js
@@ -7,3 +7,28 @@ describe('Array', function() {
     })
   })
 })
+
+describe('assert', function () {
+  it('a和b应当深度相等', function () {
+    var a = {
+      c: {
+        e: 1
+      }
+    }
+    var b = {
+      c: {
+        e: 1
+      }
+    }
+    // 修改下面代码使得满足测试描述
+    assert.equal(a, b)
+  })
+
+  it('可以捕获并验证函数fn的错误', function () {
+    function fn() {
+      xxx;
+    }
+    // 修改下面代码使得满足测试描述
+    fn()
+  })
+})
