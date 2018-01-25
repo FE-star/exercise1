38aa4fa87c813d4a0372f810ba400d023ebe0483
Ivan <15101126742@163.com>
2018-01-25 09:22:19 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      // assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题 */) // 只要不是 1，2，3 中的值，都可以通过测试用例
     })
   })
 })
@@ -21,7 +22,8 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    // assert.equal(a, b) // 需要使用 深度相等 deepEqual
+    assert.deepEqual(a, b)
   })
 
   it('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +31,9 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    // fn()
+    assert.throws(fn, Error) // 抛出的错误符合某个构造函数
   })
 })
+
+
