19ee5d06b99ee5e22562f85402c80358e7352fb2
PennyOrAmy <penny_kay@126.com>
2018-01-26 23:48:52 +0800

    hand in homework

--- test/test.js
+++ test/test.js
@@ -3,13 +3,13 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
 })
 
-describe('assert', function () {
-  it('a和b应当深度相等', function () {
+describe('assert', function() {
+  it('a和b应当深度相等', function() {
     var a = {
       c: {
         e: 1
@@ -21,14 +21,24 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepEqual(a, b)
   })
 
-  it('可以捕获并验证函数fn的错误', function () {
+  it('可以捕获并验证函数fn的错误', function() {
     function fn() {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(
+      fn => {
+        throw new Error('Wrong value');
+      },
+      function(err) {
+        if ((err instanceof Error) && /value/.test(err)) {
+          return true;
+        }
+      },
+      'unexpected error'
+    );
   })
 })
