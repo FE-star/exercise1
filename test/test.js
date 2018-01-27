2a955b07f7de850cc5adeddc093a72b1c53c6876
Tiny <453616448@qq.com>
2018-01-27 18:00:46 +0800

    update

--- test/test.js
+++ test/test.js
@@ -3,13 +3,31 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
 })
 
 describe('assert', function () {
   it('a和b应当深度相等', function () {
+    var getDepth = function(head) {
+      var allPath = [];
+      var path = [];
+      var digDepth = function(node, path) {
+            path.push(node);
+        if(typeof node !== 'object') {
+          allPath.push(path);
+          path = [head];
+        }
+        for(var p in node) {
+          if(node.hasOwnProperty(p)) {
+            digDepth(node[p], path);
+          }
+        }
+      }
+      digDepth(head, path);
+      return allPath.map(v => v.length).sort().reverse()[0];
+    }
     var a = {
       c: {
         e: 1
@@ -21,7 +39,7 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.equal(getDepth(a), getDepth(b));
   })
 
   it('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +47,6 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(function() {fn()});
   })
 })
