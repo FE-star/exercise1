a451ea8ad78b35696733dcc99aebbdda32783023
Lucy 施 <lucyshi@192.168.0.100>
2018-01-27 20:45:39 +0800

    exercise1已完成练习

--- test/test.js
+++ test/test.js
@@ -1,34 +1,42 @@
 var assert = require('assert')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题 */)
+        })
     })
-  })
 })
 
 describe('assert', function () {
-  it('a和b应当深度相等', function () {
-    var a = {
-      c: {
-        e: 1
-      }
-    }
-    var b = {
-      c: {
-        e: 1
-      }
-    }
-    // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
+    it('a和b应当深度相等', function () {
+        var a = {
+            c: {
+                e: 1
+            }
+        }
+        var b = {
+            c: {
+                e: 1
+            }
+        }
+        // 修改下面代码使得满足测试描述
+        assert.deepEqual(a, b)
+    })
 
-  it('可以捕获并验证函数fn的错误', function () {
-    function fn() {
-      xxx;
-    }
-    // 修改下面代码使得满足测试描述
-    fn()
-  })
+    it('可以捕获并验证函数fn的错误', function () {
+        function fn() {
+            xxx;
+        }
+
+
+        // 修改下面代码使得满足测试描述
+        assert.throws(
+            function () {
+                fn();
+                throw new Error("错误值");
+            },
+            Error
+        )
+    })
 })
