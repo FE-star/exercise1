2e932281c285ac7fded8962b20c11835c0b18d3f
liuwenyu <174927228@qq.com>
2018-02-03 12:05:48 +0800

    测试ci是否全检查

---
+++ test2.js
@@ -0,0 +1,40 @@
+var assert = require('assert')
+
+describe('Array2', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题 */)
+    })
+  })
+})
+
+describe('assert2', function () {
+  it('a和b应当深度相等', function () {
+    var a = {
+      c: {
+        e: 1
+      }
+    }
+    var b = {
+      c: {
+        e: 1
+      }
+    }
+    // 修改下面代码使得满足测试描述
+    assert.deepEqual(a, b)
+  })
+
+  it('可以捕获并验证函数fn的错误2', function () {
+    function fn() {
+      xxx;
+    }
+    // 修改下面代码使得满足测试描述
+    // fn()
+    assert.throws(
+        () => {
+      throw new Error('错误信息');
+  },
+    Error
+    );
+  })
+})
