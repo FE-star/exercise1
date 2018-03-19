22bed013b42ab4eb4cfa20dc40b630c043c4449c
zhangjizhe <zhang@1992>
2018-03-19 10:59:01 +0800

    测试

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题哈哈 */)
     })
   })
 })
@@ -12,6 +12,7 @@ describe('assert', function () {
     var a = {
       c: {
         e: 1
+
       }
     }
     var b = {
