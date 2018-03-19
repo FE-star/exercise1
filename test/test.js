908a9779d2e0e1f2cb9d67c1e2d5d2bb3171cc15
zhangjizhe <zhang@1992>
2018-03-19 11:00:35 +0800

    测试

--- test/test.js
+++ test/test.js
@@ -3,10 +3,11 @@ var assert = require('assert')
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题哈哈 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4)  /* 填空题哈哈 */)
     })
   })
 })
+
 describe('assert', function () {
   it('a和b应当深度相等', function () {
     var a = {
