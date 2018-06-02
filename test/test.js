11aebb14b949b69b3dd6f2a2f81678c41e67a0e4
zChange <541226467@qq.com>
2018-06-02 11:01:09 +0800

    🦆 exercise1 通过

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require('assert')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4)/* 填空题 */)
     })
   })
 })
@@ -21,14 +21,24 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
+    // assert.deepEqual(a, b) // 废弃
+    assert.deepStrictEqual(a, b)
+  });
 
   it('可以捕获并验证函数fn的错误', function () {
     function fn() {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(
+      fn,
+      (err) => {
+        if (/xxx is not defined/.test(err)) {
+          return true;
+        }
+      },
+      '未捕获到错误'
+    );
+
   })
 })
