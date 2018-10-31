2f7e090a30df810d78b12e6e996ac1f5ff37c2b8
cindylugd <cindy.lu@greendotcorp.com>
2018-10-31 15:17:38 +0800

    2018/10/31 update test cases

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3],-1)
     })
   })
 })
@@ -21,14 +21,17 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepEqual(a, b)
   })
 
   it('可以捕获并验证函数fn的错误', function () {
-    function fn() {
-      xxx;
-    }
-    // 修改下面代码使得满足测试描述
-    fn()
+	  function fn() {
+			xxx;
+		}
+		
+    assert.throws((fn)=>{
+			throw new Error('Wrong value');
+		})
+	
   })
 })
