e6a451ec1634604fa7ac29929a41fee3b484c185
zhangxuemei <zhangxuemei@360.net>
2018-10-31 13:25:51 +0800

    fork and update

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
-var assert = require('assert')
+var assert = require('assert');
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
-    })
-  })
-})
+      assert.equal([1, 2, 3].indexOf(0), -1);
+    });
+  });
+});
 
 describe('assert', function () {
   it('a和b应当深度相等', function () {
@@ -14,21 +14,21 @@ describe('assert', function () {
       c: {
         e: 1
       }
-    }
+    };
     var b = {
       c: {
         e: 1
       }
-    }
+    };
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
+    assert.equal(JSON.stringify(a).length, JSON.stringify(b).length);
+  });
 
   it('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      xxx;
-    }
+      return 'xxx';
+    };
     // 修改下面代码使得满足测试描述
-    fn()
-  })
-})
+    fn();
+  });
+});
