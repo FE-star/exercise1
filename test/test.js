40ac4608412363afcd4f68e6dbb807c7fbd06fc4
keyiwu <24648223+keyiwu@users.noreply.github.com>
2018-10-31 13:45:50 +0800

    添加一个断言解决function测试

--- test/test.js
+++ test/test.js
@@ -28,9 +28,10 @@ describe('assert', function () {
 
   it('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      // xxx;
+      xxx;
     }
     // 修改下面代码使得满足测试描述
     fn()
+    assert.throw(fn())
   })
 })
