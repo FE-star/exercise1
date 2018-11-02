617abcb496527b7c3da384e26158ba834bb79fb2
linjinxuan <jinxuan_lin@kingdee.com>
2018-11-02 16:01:53 +0800

    fix the third test

--- test/test.js
+++ test/test.js
@@ -29,10 +29,11 @@ describe('assert', function() {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    try {
-      fn();
-    } catch (err) {
-      assert.ifError(err);
-    }
+    assert.throws(fn, Error);
+    // try {
+    //   fn();
+    // } catch (err) {
+    //   assert.ifError(err);
+    // }
   });
 });
