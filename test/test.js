bcdd812fef58e9d40138348419050231a63286cd
cindylugd <cindy.lu@greendotcorp.com>
2018-11-02 15:00:59 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3],-1)
+      assert.equal(-1, [1, 2, 3].indexOf(4),-1)
     })
   })
 })
