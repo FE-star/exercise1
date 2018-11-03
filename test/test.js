a4893ddbe79cfd50ae96926ab6756bc0c449f6ff
yuchao.feng <yuchao.feng@yiducloud.cn>
2018-11-03 21:41:36 +0800

    complete exercise

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, -1)
     })
   })
 })
@@ -21,12 +21,12 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepEqual(a, b)
   })
 
   it('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      xxx;
+      return this;
     }
     // 修改下面代码使得满足测试描述
     fn()
