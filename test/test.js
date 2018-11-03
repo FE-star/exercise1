949abbd279bcf82e14d2e0c5ba015b517f141253
myname <myemial@mymail.com>
2018-11-03 14:23:18 +0800

    练习一提交

--- test/test.js
+++ test/test.js
@@ -1,34 +1,37 @@
-var assert = require('assert')
+var assert = require('assert');
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
-    })
-  })
-})
+      assert.equal(-1, [1, 2, 3].indexOf(4) /* 填空题 */);
+    });
+  });
+});
 
-describe('assert', function () {
-  it('a和b应当深度相等', function () {
+describe('assert', function() {
+  it('a和b应当深度相等', function() {
     var a = {
       c: {
         e: 1
       }
-    }
+    };
     var b = {
       c: {
         e: 1
       }
-    }
+    };
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
+    assert.deepStrictEqual(a, b);
+  });
 
-  it('可以捕获并验证函数fn的错误', function () {
+  it('可以捕获并验证函数fn的错误', function() {
     function fn() {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
-  })
-})
+
+    // fn();
+
+    assert.throws(fn, { name: 'ReferenceError' });
+  });
+});
