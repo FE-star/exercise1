f16abc928002b5afa68f116dc3e975c82153ee17
yuanfei jie <yuanfeijie@yuanfeideMacBook-Pro.local>
2018-11-03 19:44:56 +0800

    jyf练习提交

--- test/test.js
+++ test/test.js
@@ -1,34 +1,48 @@
 var assert = require('assert')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4))
+        })
     })
-  })
 })
 
-describe('assert', function () {
-  it('a和b应当深度相等', function () {
-    var a = {
-      c: {
-        e: 1
-      }
-    }
-    var b = {
-      c: {
-        e: 1
-      }
-    }
-    // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
 
-  it('可以捕获并验证函数fn的错误', function () {
-    function fn() {
-      xxx;
-    }
-    // 修改下面代码使得满足测试描述
-    fn()
-  })
+describe('assert', function () {
+    it('a和b应当深度相等', function () {
+        var a = {
+            c: {
+                e: 1
+            }
+        }
+        var b = {
+            c: {
+                e: 1
+            }
+        }
+        // 修改下面代码使得满足测试描述
+        assert.deepEqual(a, b)
+    })
+
+    it('可以捕获并验证函数fn的错误', function () {
+        function fn() {
+            xxx;
+        }
+
+        // 修改下面代码使得满足测试描述
+
+        assert.throws(
+            () => {
+
+                fn()
+
+            }, function (err) {
+
+                return true;
+
+            },
+
+            '不是期望的错误');
+    })
 })
