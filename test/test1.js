1985e6cf7c9dc8e5130b87073a6c4b9ff740f381
zhaoyan123456789 <1269894710@qq.com>
2018-11-03 10:49:40 +0800

    123

---
+++ test/test1.js
@@ -0,0 +1,34 @@
+var assert = require('assert')
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+    })
+  })
+})
+
+describe('assert', function () {
+  it('a和b应当深度相等', function () {
+    var a = {
+      c: {
+        e: 1
+      }
+    }
+    var b = {
+      c: {
+        e: 1
+      }
+    }
+    // 修改下面代码使得满足测试描述
+    assert.equal(a, b)
+  })
+
+  it('可以捕获并验证函数fn的错误', function () {
+    function fn() {
+      xxx;
+    }
+    // 修改下面代码使得满足测试描述
+    fn()
+  })
+})
