c61b144a15711356851ebef0e34a9f94fd8ab5e6
moling <moling@moling.com>
2019-03-13 16:10:51 +0800

    first exercise01

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require('assert')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(0) )
     })
   })
 })
@@ -21,7 +21,7 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepStrictEqual(a, b)
   })
 
   it('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +29,7 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(fn, 'fun error');
   })
-})
+});
+
