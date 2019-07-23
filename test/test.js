50240466094161de3d2e56f45366ed014f75f3b2
MartinsYong <a545067127@outlook.com>
2019-07-23 17:20:25 +0800

    DONE

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
 })
@@ -14,14 +14,14 @@ describe('assert', function () {
       c: {
         e: 1
       }
-    }
+    };
     var b = {
       c: {
         e: 1
       }
-    }
+    };
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepStrictEqual(a, b)
   })
 
   it('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +29,10 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    try {
+	fn();
+    } catch (e) {
+	assert(e instanceof ReferenceError);
+    }
   })
 })
