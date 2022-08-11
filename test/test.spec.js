bae63055d45b448268776d7e155e507830bae972
xiang.shen <xiang.shen@shopee.com>
2022-08-11 10:16:38 +0800

    feat: 测试通过

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,27 +1,27 @@
-var assert = require('assert')
+const assert = require('assert')
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(Infinity))
     })
   })
 })
 
 describe('assert', function () {
   test('a和b应当深度相等', function () {
-    var a = {
+    const a = {
       c: {
         e: 1
       }
     }
-    var b = {
+    const b = {
       c: {
         e: 1
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepEqual(a, b)
   })
 
   test('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +29,9 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(fn, {
+      name: "ReferenceError",
+      message: 'xxx is not defined',
+    })
   })
 })
