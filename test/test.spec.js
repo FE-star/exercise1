011381541ae2c9c80b66f71421795f678deae119
chenli <chenl@homeking365.com>
2022-08-12 13:12:40 +0800

    feat: 完成作业

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,9 +1,9 @@
 var assert = require('assert')
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    test('当value不在数组中应当返回-1', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
 })
@@ -21,14 +21,18 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.equal(JSON.stringify(a), JSON.stringify(b))
   })
 
   test('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      xxx;
+      xxx
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    try {
+      fn()
+    } catch (error) {
+      console.error(error)
+    }
   })
 })
