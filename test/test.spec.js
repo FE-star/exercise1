3a2fbb15771e145b5976de31ceba52ae62ca8042
西岚 <5491245+silanlee@user.noreply.gitee.com>
2022-08-13 19:58:45 +0800

    feat:完成测试用例

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,12 @@
-var assert = require('assert')
+var assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
-    })
-  })
-})
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    test('当value不在数组中应当返回-1', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4) /* 填空题 */);
+    });
+  });
+});
 
 describe('assert', function () {
   test('a和b应当深度相等', function () {
@@ -14,21 +14,25 @@ describe('assert', function () {
       c: {
         e: 1
       }
-    }
+    };
     var b = {
       c: {
         e: 1
       }
-    }
+    };
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
+    assert.deepEqual(a, b);
+  });
 
   test('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      xxx;
+      // xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
-  })
-})
+    try {
+      fn();
+    } catch (err) {
+      assert.throws(err);
+    }
+  });
+});
