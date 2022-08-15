76d144691185fdcf29f22d9227885888dc1426cf
xiaoquan <xiaoquan@qunhemail.com>
2022-08-15 21:42:33 +0800

    complete exercise1

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
 })
@@ -21,7 +21,7 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepStrictEqual(a, b)
   })
 
   test('可以捕获并验证函数fn的错误', function () {
@@ -29,6 +29,8 @@ describe('assert', function () {
       xxx;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    assert.throws(() => {
+      fn()
+    }, Error)
   })
 })
