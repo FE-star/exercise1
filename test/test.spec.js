03541853b3948316af28ccd21184f1dc73d757fe
xiaoquan <xiaoquan@qunhemail.com>
2022-08-15 21:46:09 +0800

    complete exercise1

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3].indexOf(4))
+      assert.equal(-1, [1, 2, 3].indexOf(0))
     })
   })
 })
