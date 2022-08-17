52fcc3cbddd7753acefe6cd1d6eff15430175d61
WB20210831001 <yangyang05@sinosoft.com.cn>
2022-08-17 20:31:17 +0800

    第一次作业提交

--- test/test.spec.js
+++ test/test.spec.js
@@ -3,7 +3,7 @@ var assert = require('assert')
 describe('Array', function() {
   describe('#indexOf()', function() {
     test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
+      assert.equal(-1, [1, 2, 3].indexOf(5)/* 填空题 */)
     })
   })
 })
@@ -21,14 +21,16 @@ describe('assert', function () {
       }
     }
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
+    assert.deepEqual(a, b)
   })
 
   test('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      xxx;
+      // xxx;
+      throw err;
     }
     // 修改下面代码使得满足测试描述
-    fn()
+    
+    assert.throws(fn,)
   })
 })
