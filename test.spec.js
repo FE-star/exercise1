c5bb2632ce19ea5ceaceb01720c761ea75c72aac
kexinxu <1760209720@qq.com>
2022-08-17 18:11:54 +0800

    exercise1

---
+++ test.spec.js
@@ -0,0 +1,39 @@
+var assert = require('assert')
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    test('当value不在数组中应当返回-1', function() {
+      assert.equal(-1, [1, 2, 3].indexOf(4))
+    })
+  })
+})
+
+describe('assert', function () {
+  test('a和b应当深度相等', function () {
+    var a = {
+      c: {
+        e: 1
+      }
+    }
+    var b = {
+      c: {
+        e: 1
+      }
+    }
+    // 修改下面代码使得满足测试描述
+    assert.deepEqual(a, b)
+  })
+
+  test('可以捕获并验证函数fn的错误', function () {
+    function fn() {
+      throw new Error("Wrong value");
+    }
+    // 修改下面代码使得满足测试描述 
+
+    assert.throws(
+      fn,
+      Error,
+      '不符合预期的错误类型'
+    )
+  })
+})
