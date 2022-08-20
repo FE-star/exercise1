88bf56ccd18a131f67a5d6940ef625bdc33482c3
Chris Wang <chris.wang@tcam.co>
2022-08-21 01:42:35 +1000

    Update test

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,34 +1,44 @@
-var assert = require('assert')
+var assert = require('assert');
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    test('当value不在数组中应当返回-1', function() {
-      assert.equal(-1, [1, 2, 3]/* 填空题 */)
-    })
-  })
-})
+const deepEqual = (x, y) => {
+  if (x === y) return true;
+  if (typeof x !== 'object' || typeof y !== 'object') return false;
+  if (x.length !== y.length) return false;
+  for (var i = 0; i < x.length; i++) {
+    if (!deepEqual(x[i], y[i])) return false;
+  }
+  return true;
+};
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    test('当value不在数组中应当返回-1', function () {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
 
 describe('assert', function () {
   test('a和b应当深度相等', function () {
     var a = {
       c: {
-        e: 1
-      }
-    }
+        e: 1,
+      },
+    };
     var b = {
       c: {
-        e: 1
-      }
-    }
+        e: 1,
+      },
+    };
     // 修改下面代码使得满足测试描述
-    assert.equal(a, b)
-  })
+    assert.deepEqual(a, b);
+  });
 
   test('可以捕获并验证函数fn的错误', function () {
     function fn() {
-      xxx;
+      throw err;
     }
     // 修改下面代码使得满足测试描述
-    fn()
-  })
-})
+    assert.throws(fn);
+  });
+});
